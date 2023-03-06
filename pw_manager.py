48848427fa10ed4222efb3244d08674aa3eff7bd
Max Surnin <78266359+MadMax8930@users.noreply.github.com>
2023-03-06 22:28:16 +0100

    updt

--- pw_manager.py
+++ pw_manager.py
@@ -1,1 +1,23 @@
-test
+pwd = input("What is the master password? ")
+
+def view():
+    pass
+
+def add():
+    name = input('Account Name: ')
+    pwd = input("Password: ")
+
+    with open('passwords.txt', 'a') as f:
+
+while True:
+    mode = input("Would you like to add a new password or view existing ones (view, add), press q to quit? ").lower()
+    if mode == "q":
+        break
+
+    if mode == "view":
+        view()
+    elif mode == "add":
+        add()
+    else:
+        print("Ivalid mode.")
+        continue
